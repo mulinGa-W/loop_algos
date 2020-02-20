64b6fd173630203897beff8d5d16b4c902b7d66d
mulinGa-W <mulinga@gmail.com>
2020-02-19 18:20:26 -0800

    Updated

--- 3_fizzbuzz.js
+++ 3_fizzbuzz.js
@@ -6,8 +6,19 @@
 
 //The expected output will be 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz .... etc 
 
+for(var i = 1; i < 31; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("Fizz Buzz");
+    }
+    else if(i % 3 == 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz")
+    }
+    else
+    {
+    console.log(i);
+    }
+}
 
-
-
-
-//YOUR CODE HERE
